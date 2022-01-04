2f4ceb9133d6be2de4ada2c4c9158cd89ec40a67
shasheen <shasheenbandodkar98@gmail.com>
2022-01-03 22:00:01 -0800

    changes

--- Prep-Projects/cipher.py
+++ Prep-Projects/cipher.py
@@ -7,7 +7,7 @@
 from base64 import b64encode, b64decode
 import hashlib
 import os 
-from Cryptodome.Cipher import AES
+from Crytodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 
 
