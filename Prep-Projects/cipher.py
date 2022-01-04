89f498a5e3134bcbe7ec2975c6e5f583a9021411
shasheen <shasheenbandodkar98@gmail.com>
2022-01-03 18:50:29 -0800

    added comment

--- Prep-Projects/cipher.py
+++ Prep-Projects/cipher.py
@@ -1,2 +1,8 @@
 #testing for git 
-# trying to implement a basic cipher
+# trying to implement a basic cipher. Implementation of 256 AES Cipher
+# Certain concepts to keep in mind. Galois/Counter Mode. 
+# padding is important for keeping encrytpion strong and unpredictable. 
+
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
