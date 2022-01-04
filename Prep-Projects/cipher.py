027037bc110b8cb110f00e463673815f25b3a69b
shasheen <shasheenbandodkar98@gmail.com>
2022-01-03 21:35:47 -0800

    new

--- Prep-Projects/cipher.py
+++ Prep-Projects/cipher.py
@@ -5,9 +5,10 @@
 
 from base64 import b64encode, b64decode
 import hashlib
-from Cryptodome.Cipher import AES
 import os 
-from Crytodome.Random import get_random_bytes
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
 
 def encrypt(plain_text, password):
     #generate random salt
