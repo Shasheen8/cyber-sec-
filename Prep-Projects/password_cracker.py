79f8664c8ecc44133b17104c65720342114a5b8e
shasheen <shasheenbandodkar98@gmail.com>
2022-01-07 19:00:30 -0800

    passwd file

--- Prep-Projects/password_cracker.py
+++ Prep-Projects/password_cracker.py
@@ -5,14 +5,26 @@
 import crypt 
 import optparse
 
-def test_password(crypt_password, dname):
+def test_password(crypt_password, dict_name):
     salt = crypt_password[0:2]
-    dict_File = open(dname, 'r')
+    dict_File = open(dict_name, 'r')
     for word in dict_File.readlines():
         word = word.strip ('\n')
         cryptWorld = crypt.crypt(word,salt)
         if (cryptWorld == crypt_password):
-            print "Password Cracked: "+word+"\n"
+            print "Password Found: "+word+"\n"
             return
     print "Password Not Found. \n"
     return
+
+def main():
+    passwordFile = open(password_name)
+    for line in passwordFile.readlines():
+        if ":" in line:
+            user = line.split (":")[0]
+            crypt_password = line.split(":")[1].strip('')
+            print "Cracking Password: "+user
+            test_password(crypt_password, dict_name)
+
+
+main()
