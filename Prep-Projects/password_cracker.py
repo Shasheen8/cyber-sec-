3f1abb285fb55ac5c6a643c26079fe9b81a3b896
shasheen <shasheenbandodkar98@gmail.com>
2022-01-07 18:02:10 -0800

    password cracker

--- Prep-Projects/password_cracker.py
+++ Prep-Projects/password_cracker.py
@@ -4,3 +4,15 @@
 
 import crypt 
 import optparse
+
+def test_password(crypt_password, dname):
+    salt = crypt_password[0:2]
+    dict_File = open(dname, 'r')
+    for word in dict_File.readlines():
+        word = word.strip ('\n')
+        cryptWorld = crypt.crypt(word,salt)
+        if (cryptWorld == crypt_password):
+            print "Password Cracked: "+word+"\n"
+            return
+    print "Password Not Found. \n"
+    return
