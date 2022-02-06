3d7bf07aa228d39641cc8de6af22da554b4850b2
shasheen <shasheenbandodkar98@gmail.com>
2022-02-05 22:42:36 -0800

    beautiful-soup csv format

--- Prep-Projects/web_scraper.py
+++ Prep-Projects/web_scraper.py
@@ -1,5 +1,6 @@
 #implementing a web scraper
 
+from ipaddress import summarize_address_range
 from bs4 import BeautifulSoup
 import csv
 import requests
@@ -7,3 +8,14 @@ import requests
 source = requests.get('https://healthybyte.net').text
 
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open ('','')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline','summary','video_link'])
+
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print (headline)
+
+    summary = article.find('div',class='youtube-player')['src']
