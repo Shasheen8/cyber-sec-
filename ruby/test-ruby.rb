17076e4c6d555baa86b45a51faa48eb52e66be4a
shasheen <shasheenbandodkar98@gmail.com>
2022-06-26 22:30:28 -0700

    feat(ruby): added a ruby file

---
+++ ruby/test-ruby.rb
@@ -0,0 +1,29 @@
+# name = "Shasheen"
+# age = 28
+# is_male = true
+# is_fat  = false
+
+# if (is_male == false) 
+#     puts (name + " is a bad boy")
+# else 
+#     puts (name + " is a good boy")
+# end
+
+# if (is_fat == false) 
+#     puts (name + " is a skinny")
+# else 
+#     puts (name.upcase + " is a fat")
+# end
+
+# puts name.include? "Shash"
+# puts name[0]
+# puts name.index ("een")
+
+# puts "lalaland".upcase()
+
+# num = 20.65868
+# puts num.round()
+
+puts "Enter your phone: "
+number = gets.chomp()
+puts ("Gotcha " + number + "!")
